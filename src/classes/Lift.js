b2d015a67f42c3a5b83b005abdcd8a1c14471d8b
Alexander Rolf <ar223hh@student.lnu.se>
2024-10-10 17:53:19 +0200

    Implement validation in the constructor as Daniel told us it is best practice

--- src/classes/Lift.js
+++ src/classes/Lift.js
@@ -19,10 +19,59 @@ class Lift {
    * @param {number} sets       How many sets did the user perform.
    */
   constructor (lift, weight, weightUnit, reps, sets) {
+    this.validateLift(lift)
+    this.validateWeight(weight)
+    this.validateReps(reps)
+    this.validateSets(sets)
+
     this.#lift = lift
     this.#weight = new Weight(weight, weightUnit)
     this.#reps = reps
     this.#sets = sets
+  }
+
+  /**
+   * This method validates the lift to make sure it is a string and not empty.
+   *
+   * @param {string} lift The lift to validate.
+   */
+  validateLift (lift) {
+    if (typeof lift !== 'string' || lift.length === 0) {
+      throw new Error('You must enter the lift you performed and not leave it empty!')
+    }
+  }
+
+  /**
+   * This method validates the weight to make sure it is a number and not less than or equal to 0.
+   *
+   * @param {number} weight The weight to validate.
+   */
+  validateWeight (weight) {
+    if (typeof weight !== 'number' || weight <= 0) {
+      throw new Error('You have to enter the weight you lifted!')
+    }
+  }
+
+  /**
+   * This method validates the reps to make sure it is a number and not less than or equal to 0.
+   *
+   * @param {number} reps The reps to validate.
+   */
+  validateReps (reps) {
+    if (typeof reps !== 'number' || reps <= 0) {
+      throw new Error('You have to enter the reps you performed!')
+    }
+  }
+
+  /**
+   * This method validates the sets to make sure it is a number and not less than or equal to 0.
+   *
+   * @param {number} sets The sets to validate.
+   */
+  validateSets (sets) {
+    if (typeof sets !== 'number' || sets <= 0) {
+      throw new Error('You have to enter the sets you performed!')
+    }
   }
 
   /**
@@ -32,6 +81,7 @@ class Lift {
    * @param {string} updatedWeightUnit The new weight unit of the lift.
    */
   updateLiftWeight (updatedWeight, updatedWeightUnit) {
+    this.validateWeight(updatedWeight)
     this.#weight = new Weight(updatedWeight, updatedWeightUnit)
   }
 
@@ -41,6 +91,7 @@ class Lift {
    * @param {number} updatedReps The new reps of the lift.
    */
   updateLiftReps (updatedReps) {
+    this.validateReps(updatedReps)
     this.#reps = updatedReps
   }
 
@@ -50,6 +101,7 @@ class Lift {
    * @param {number} updatedSets The new sets of the lift.
    */
   updateLiftSets (updatedSets) {
+    this.validateSets(updatedSets)
     this.#sets = updatedSets
   }
 
