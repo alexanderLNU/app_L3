8e052252f33b444ad2123035950ff53df9a58ff4
Alexander Rolf <ar223hh@student.lnu.se>
2024-10-13 11:59:08 +0200

    Add getter for the lfits in a session and also add JSDoc

--- src/classes/LiftSession.js
+++ src/classes/LiftSession.js
@@ -20,18 +20,40 @@ class LiftSession {
     this.#nameOfSession = nameOfSession
   }
 
-  // Add the lfit to the session.
+  /**
+   * This method returns all the lifts registered in that session.
+   *
+   * @returns {Array} An array of all the lifts registered in that session.
+   */
+  getSessionLifts () {
+    return this.#sessionLifts.map(lift => lift.getLiftStats())
+  }
+
+  /**
+   * This method is adding a lift to the session.
+   *
+   * @param {Lift} lift The lift that should be added to the session.
+   */
   addLiftToSession (lift) {
     this.validateLiftInstance(lift)
     this.#sessionLifts.push(lift)
     this.addLiftToCollection(lift)
   }
 
-  // Add the lift to the collection.
+  /**
+   * This method adds the weight of lift to the collection of weights.
+   *
+   * @param {Lift} lift The lift we extract the weight from to add to collection.
+   */
   addLiftToCollection (lift) {
     this.#weightCollection.addWeightToCollection(lift.getWeight())
   }
 
+  /**
+   * This method validates if lift is a valid instance of lift (the class).
+   *
+   * @param {Lift} lift The lfit that is being validated.
+   */
   validateLiftInstance (lift) {
     if (!(lift instanceof Lift)) {
       throw new Error('You must enter a valid lift instance!')
