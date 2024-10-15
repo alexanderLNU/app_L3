bddc8038c9c9b68bc20a079c83ce655406ee144e
Alexander Rolf <ar223hh@student.lnu.se>
2024-10-15 13:56:49 +0200

    Fix JSDoc for the PerformanceLog

--- src/classes/PerformanceLog.js
+++ src/classes/PerformanceLog.js
@@ -10,31 +10,60 @@ class PerformanceLog {
   #collectionOfSessions
   #weightCollection
 
+  /**
+   * This constructor creates a new instance of PerformanceLog.
+   */
   constructor () {
     this.#collectionOfSessions = []
     this.#weightCollection = new WeightCollection()
   }
 
+  /**
+   * This method returns how many sessions it is in the PerformanceLog.
+   *
+   * @returns {number} The number of sessions in the PerformanceLog.
+   */
   getNumberOfSessions () {
     return this.#collectionOfSessions.length
   }
 
+  /**
+   * This method returns the heaviest lift in the PerformanceLog.
+   *
+   * @returns {string} The heaviest lift in the PerformanceLog.
+   */
   getHeaviestLift () {
     this.validateLogNotEmpty()
     return this.#weightCollection.getHeaviestWeight().toString()
   }
 
+  /**
+   * This method returns the lightest lift in the PerformanceLog.
+   *
+   * @returns {string} The lightest lift in the PerformanceLog.
+   */
   getLightestLift () {
     this.validateLogNotEmpty()
     return this.#weightCollection.getLightestWeight().toString()
   }
 
+  /**
+   * This method returns the average weight for all of the lifts in the unit user has chosen.
+   *
+   * @param {string} weightUnit The unit chosen for the average weight and is by default in kg.
+   * @returns {string} Average weight as a string and it is two decimal places.
+   */
   getAverageWeightOfAllLifts (weightUnit = 'kg') {
     this.validateLogNotEmpty()
     const averageWeight = this.#weightCollection.getAverageWeight(weightUnit).toString()
     return parseFloat(averageWeight).toFixed(2) + ' ' + weightUnit
   }
 
+  /**
+   * This method adds all lifts from a LiftSession to the collection of weights.
+   *
+   * @param {LiftSession} liftSession The LiftSession that lifts should be added from.
+   */
   addLiftSessionWeightsToCollection (liftSession) {
     const lifts = liftSession.getSessionLifts()
     lifts.forEach(lift => {
@@ -43,6 +72,11 @@ class PerformanceLog {
     })
   }
 
+  /**
+   * This method adds a LiftSession to the PerformanceLog.
+   *
+   * @param {LiftSession} liftSession The LiftSession that should be added to the PerformanceLog.
+   */
   addLiftSession (liftSession) {
     this.validateSessionInstance(liftSession)
     this.#collectionOfSessions.push(liftSession)
