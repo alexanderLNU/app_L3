ff4e48c9d9e2351d03db23debfc43a15c620e860
Alexander Rolf <ar223hh@student.lnu.se>
2024-10-17 19:51:38 +0200

    Implement Menu further with start and name future methods for the menu class

--- src/classes/Menu.js
+++ src/classes/Menu.js
@@ -3,11 +3,53 @@ import Lift from './Lift.js'
 import LiftSession from './LiftSession.js'
 import PerformanceLog from './PerformanceLog.js'
 
+// JSDoc fixa imon.
 class Menu {
   #performanceLog
 
   constructor() {
     this.#performanceLog = new PerformanceLog()
+  }
+
+  static startTheApp () {
+    const menu = new Menu()
+    menu.startMenu()
+  }
+
+  startMenu () {
+    console.log('Hello, Welcome to "Alexander Strength Lifting Tool"!')
+    let quitTheApp = false
+
+    while (!quitTheApp) {
+      console.log('\nMain Menu:')
+      console.log('1. Add new lifting session')
+      console.log('2. View your stats')
+      console.log('3. View all lifting sessions')
+      console.log('4. Remove a lifting session')
+      console.log('5. Quit the app')
+      const usersChoice = readlineSync.question('Choose one of the following (1-5): ')
+
+      switch (usersChoice) {
+        case '1':
+          this.addNewLiftingSession() // Fixa imon
+          break
+        case '2':
+          this.viewPerformanceStats() // Fixa imon
+          break
+        case '3':
+          this.viewAllLiftingSessions() // Fixa imon
+          break
+        case '4':
+          this.removeALiftingSession() // Fixa imon
+          break
+        case '5':
+          console.log('Quitting the app...')
+          quitTheApp = true
+          break
+        default:
+          console.log('Not a valid choice, try again.')
+      }
+    }
   }
 }
 
--- src/main.js
+++ src/main.js
@@ -1,3 +1,3 @@
-import Menu from './classes/Menu.js';
+import Menu from './classes/Menu.js'
 
-// Starta appen här sen.
+Menu.startTheApp() // Bör vara rätt, fixa alla metoder imon i Menu.
