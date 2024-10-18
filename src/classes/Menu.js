bde934035613c661311615effc9ae72cd4b87b6e
Alexander Rolf <ar223hh@student.lnu.se>
2024-10-18 16:48:18 +0200

    Implement the perforamce stats method i nMenu.js

--- src/classes/Menu.js
+++ src/classes/Menu.js
@@ -76,9 +76,55 @@ class Menu {
     this.#performanceLog.addLiftSession(liftingSession)
     console.log('You have successfully added the lifting session!')
   }
-  // stats
+
+  viewPerformanceStats () {
+    this.validateSessionIsPresent()
+    console.log('\nYour performance stats:')
+    console.log('1. The heaviest lift out of all lifts')
+    console.log('2. The lightest lift out of all lifts')
+    console.log('3. The average weight out of all lifts')
+    console.log('4. The heaviest lift of a specific lift')
+    console.log('5. The lightest lift of a specific lift')
+    console.log('6. The average weight of a specific lift')
+    const usersChoice = readlineSync.question('Choose one of the following (1-6): ')
+
+    switch (usersChoice) {
+      case '1':
+        console.log('The heaviest lift out of all lifts is:', this.#performanceLog.getHeaviestLift())
+        break
+      case '2':
+        console.log('The lightest lift out of all lifts is:', this.#performanceLog.getLightestLift())
+        break
+      case '3':
+        console.log('The average weight out of all lifts is:', this.#performanceLog.getAverageWeightOfAllLifts())
+        break
+      case '4': {
+        const nameOfHeaviest = readlineSync.question('Enter the name of the lift: ')
+        console.log('The heaviest lift of', nameOfHeaviest, 'is:', this.#performanceLog.getHeaviestLiftForSpecificLift(nameOfHeaviest))
+        break
+      }
+      case '5': {
+        const nameOfLightest = readlineSync.question('Enter the name of the lift: ')
+        console.log('The lightest lift of', nameOfLightest, 'is:', this.#performanceLog.getLightestLiftForSpecificLift(nameOfLightest))
+        break
+      }
+      case '6': {
+        const nameOfAverage = readlineSync.question('Enter the name of the lift: ');
+        console.log('The average weight of', nameOfAverage, 'is:', this.#performanceLog.getAverageWeightForSpecificLift(nameOfAverage))
+        break
+      }
+      default:
+        console.log('Not a valid choice, try again.')
+    }
+  }
   // viewalllift
   // removeone
+
+  validateSessionIsPresent () {
+    if (this.#performanceLog.getNumberOfSessions() === 0) {
+      throw new Error('You have no lifting sessions, you must add at least one!')
+    }
+  }
 }
 
 export default Menu
